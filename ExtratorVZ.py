a3e8c86ba450754e3cda2bf728ae7c5d0770e3ba
jhow <jhow_72@usp.br>
2022-03-24 01:32:05 -0300

    Questão dos dicionários de campos resolvida, só crio 1 agora

--- ExtratorVZ.py
+++ ExtratorVZ.py
@@ -6,29 +6,17 @@ from models import FilaVZ
 app = Flask(__name__)
 
 # essas propriedades são as que aparecerão no index inicial
-propriedades_campos_para_alteracao = {
-    'chpras': '738651124',
-    'data_transacao': '2022-10-07',
-    'hora_transacao': '12:32:41',
-    'dn': '01285',
-    'indicador_cv': 'N',
-    'cf': '02',
-    'numero_documento': '4403589281',
-    'indicador_titular': '0',
-    'nome_estabelecimento': 'Garoupa',
-    'tipo_pessoa': 'F',
-    'valor_moeda_transacao': '000000000727272',
-    'hash_cartao': 'F28BE31DBAE83DB283B',
-}
+
+propriedades_campos_para_alteracao = helpers.cria_dicionario(True)
 
 # gera as propriedades iniciais
 massa_default = '----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'
 fila = FilaVZ(massa_default)
-lista_campos = helpers.CriaPropriedades(fila, fila.String, propriedades_campos_para_alteracao)
 
 # gera a pagina inicial, nesse estágio "massa_atualizada = massa_default" pois não houve nenhuma alteração ainda
 @app.route('/')
 def index():
+    lista_campos = helpers.CriaPropriedades(fila, fila.String, propriedades_campos_para_alteracao)
     return render_template('index.html', titulo='Editor de Massas - VZ', massa_default=massa_default, lista_campos=lista_campos, massa_atualizada=massa_default)
 
 # aqui é a base da atualização da massa
@@ -37,25 +25,10 @@ def propertySet():
 
     # cria um novo dicionario que será usado para atualizar os valores da massa e manter o estado do form
     # os valores são recebidos via POST e as chaves são as mesmas que as setadas em helpers.CriaPropriedades
-    propriedades_campos_para_alteracao_atualizado = {
-        'chpras': request.form['chpras'],
-        'data_transacao': request.form['data_transacao'],
-        'hora_transacao': request.form['hora_transacao'],
-        'dn': request.form['dn'],
-        'indicador_cv': request.form['indicador_cv'],
-        'cf': request.form['cf'],
-        'numero_documento': request.form['numero_documento'],
-        'indicador_titular': request.form['indicador_titular'],
-        'nome_estabelecimento': request.form['nome_estabelecimento'],
-        'tipo_pessoa': request.form['tipo_pessoa'],
-        'valor_moeda_transacao': request.form['valor_moeda_transacao'],
-        'hash_cartao': request.form['hash_cartao'],
-    }
+    propriedades_campos_para_alteracao_atualizado = request.form
 
-    lista_campos_atualizada = helpers.CriaPropriedades(fila, fila.String, propriedades_campos_para_alteracao)
+    lista_campos_atualizada = helpers.CriaPropriedades(fila, fila.String, propriedades_campos_para_alteracao_atualizado)
     massa_atualizada = helpers.replace_values(fila, propriedades_campos_para_alteracao_atualizado, lista_campos_atualizada)
-    lista_campos_atualizada = helpers.CriaPropriedades(fila, massa_atualizada, propriedades_campos_para_alteracao_atualizado)
-
 
     # renderiza o template index.html com os valores atualizados
     return render_template('index.html', titulo='Editor de Massas - VZ', massa_default=massa_default, lista_campos=lista_campos_atualizada, massa_atualizada=massa_atualizada)
--- helpers.py
+++ helpers.py
@@ -1,12 +1,12 @@
 from models import FilaVZ, PropertyVZ
 
-
 # tem por objetivo popular os valores das propriedades que aparecerão no html do index
 # retorna uma lista com o id, name(label) e value que será usado para criar os campos do form
 def CriaPropriedades(fila, fila_string, dicionario):
 
     lista_campos = [
         PropertyVZ(fila, fila_string, 'chpras', 'CHPRAS', dicionario['chpras'], 19, 11, True, False, False),
+        PropertyVZ(fila, fila_string, 'motivo_negada', 'motivo da negada', dicionario['motivo_negada'], 65, 3, False, True, False),
         PropertyVZ(fila, fila_string,'data_transacao', 'data transação', dicionario['data_transacao'], 68, 10, False, True, False),
         PropertyVZ(fila, fila_string,'hora_transacao', 'hora transação', dicionario['hora_transacao'], 78, 8, False, True, False),
         PropertyVZ(fila, fila_string,'dn', 'DN', dicionario['dn'], 117, 5, True, False, False),
@@ -22,8 +22,33 @@ def CriaPropriedades(fila, fila_string, dicionario):
 
     return lista_campos
 
+# o dicionario abaixo, contém os valores usados como exemplos iniciais nos campos de index.html
+def cria_dicionario(novo):
+    if novo:
+        propriedades_campos_para_alteracao = {
+            'chpras': '738651124',
+            'motivo_negada': 'BND',
+            'data_transacao': '2022-10-07',
+            'hora_transacao': '12:32:41',
+            'dn': '01285',
+            'indicador_cv': 'N',
+            'cf': '02',
+            'numero_documento': '00004403589281',
+            'indicador_titular': '0',
+            'nome_estabelecimento': 'Garoupa',
+            'tipo_pessoa': 'F',
+            'valor_moeda_transacao': '000000000727272',
+            'hash_cartao': 'F28BE31DBAE83DB283B',
+        }
+        return propriedades_campos_para_alteracao
+
+
 def replace_values(fila, propriedades_campos_para_alteracao, lista_campos_atualizada):
 
+# loopa pelas chaves e valores do dicionario e por cada objeto da lista de campos atualizada
+# bate key == id, quando for igual, usa os atributos check para saber se deve fazer a chamada ou não
+# da um break no for da lista de objetos quando consegue fazer o replace
+# procurar forma mais eficiente de fazer isso
     for key, value in propriedades_campos_para_alteracao.items():
         for j, campo in enumerate(lista_campos_atualizada):
             if key == campo.id:
@@ -39,26 +64,13 @@ def replace_values(fila, propriedades_campos_para_alteracao, lista_campos_atuali
 
     return fila.String
 
-    # # chamada de cada metodo especifico que altera o valor na fila original
-    # fila.chpras_replace(propriedades_campos_para_alteracao['chpras'])
-    # fila.data_transacao_replace(propriedades_campos_para_alteracao['data_transacao'])
-    # fila.hora_transacao_replace(propriedades_campos_para_alteracao['hora_transacao'])
-    # fila.dn_replace(propriedades_campos_para_alteracao['dn'])
-    # fila.indicador_cartao_virtual_replace(propriedades_campos_para_alteracao['indicador_cv'])
-    # fila.cf_replace(propriedades_campos_para_alteracao['cf'])
-    # fila.numero_documento_replace(propriedades_campos_para_alteracao['numero_documento'])
-    # fila.indic_titularidade_replace(propriedades_campos_para_alteracao['indicador_titular'])
-    # fila.nome_estabelecimento_replace(propriedades_campos_para_alteracao['nome_estabelecimento'])
-    # fila.tipo_pessoa_replace(propriedades_campos_para_alteracao['tipo_pessoa'])
-    # fila.valor_transacao_replace(propriedades_campos_para_alteracao['valor_moeda_transacao'])
-    # fila.hash_cartao_replace(propriedades_campos_para_alteracao['hash_cartao'])
-
 
 def _replace(fila, parte_nova, propriedade_do_contexto):
     string_atualizada = _replace_handler(fila, parte_nova, propriedade_do_contexto)
     propriedade_do_contexto.valor = parte_nova
 
     fila.String = string_atualizada
+
 
 # criado um metodo auxiliar para o replace pois a funcao built-in do python não me atende
 # ou ela mudava todas as menções ao que eu estava procurando ou um número específcio
@@ -70,8 +82,6 @@ def _replace_handler(fila, parte_nova, propriedade):
 
 
 # as funcoes abaixo são usadas para validar o q for necessário antes de fazer o replace
-
-
 def _replace_check_zfill(valor_novo, objeto_atual):
     if len(valor_novo) > objeto_atual.qtd_chars:
         raise ValueError(f"{objeto_atual.id} deve ter {objeto_atual.qtd_chars} chars ou menos")
